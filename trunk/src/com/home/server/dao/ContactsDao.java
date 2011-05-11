5eca827c5effec9c2652f1c4251d72cf5519dd1c
leo.shabr@gmail.com <leo.shabr@gmail.com@4c94caee-58b1-7c66-d3c6-37a243393383>
2011-05-11 06:49:02 +0000

    getAllContacts by onToMany Mapping

--- trunk/src/com/home/server/dao/ContactsDao.java
+++ trunk/src/com/home/server/dao/ContactsDao.java
@@ -1,10 +1,11 @@
 package com.home.server.dao;
 
 import java.util.List;
+import java.util.Set;
 
 import com.home.server.entities.Contacts;
 
 public interface ContactsDao extends GenericDao<Contacts> {
 
-	public List<Contacts> getAllContacts();
+	public Set<Contacts> getAllContacts();
 }
